99c2d2f87ca1549946e5769202639e92314c81b9
Your Name Here <devyk100@gmail.com>
2024-04-02 23:21:24 +0530

    saving to postgres db

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 
 import initialVersionRouter from "./v1/routes";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const app = express()
 app.use(express.json())
 app.use("/api/v1", initialVersionRouter);
@@ -15,3 +16,11 @@ app.listen(3000, () => {
   console.log("listening on port 3000");
 })
 
+// async function clear(){
+//   await prisma.data.delete({
+//     where: {
+//       id: 1
+//     }
+//   })
+// }
+// clear()
--- src/v1/routes/sync/single.ts
+++ src/v1/routes/sync/single.ts
@@ -5,54 +5,111 @@ import { saveBase64File } from "../../fileIntercept";
 const prisma = new PrismaClient();
 const singleSyncRouter = Router();
 
-
 const dataSchema = z.object({
-    generalData: z.object({
-        latitude: z.number(),
-        longitude: z.number(),
-        accuracyCorrection: z.number(),
-        landCover: z.string(),
-        locationDescription: z.string()
-    }),
-    cropInformation: z.object({
-        waterSource: z.string(),
-        cropIntensity: z.string(),
-        primarySeason: z.string(),
-        primaryCrop: z.string(),
-        secondarySeason: z.string(),
-        secondaryCrop: z.string(),
-        livestock: z.string(),
-        croppingPattern: z.string(),
-        cropGrowthStage: z.string(),
-        remarks: z.string()
-    }),
-    CCE: z.object({
-        sampleSize1: z.number(),
-        sampleSize2: z.number(),
-        grainWeight: z.number(),
-        biomassWeight: z.number(),
-        cultivar: z.string(),
-        sowDate: z.date(),
-        harvestDate: z.date()
-    }),
-    noOfImages: z.number()
-})
+  generalData: z.object({
+    latitude: z.number(),
+    longitude: z.number(),
+    accuracyCorrection: z.number(),
+    landCover: z.string(),
+    locationDescription: z.string(),
+  }),
+  cropInformation: z.object({
+    waterSource: z.string(),
+    cropIntensity: z.string(),
+    primarySeason: z.string(),
+    primaryCrop: z.string(),
+    secondarySeason: z.string(),
+    secondaryCrop: z.string(),
+    livestock: z.string(),
+    croppingPattern: z.string(),
+    cropGrowthStage: z.string(),
+    remarks: z.string(),
+  }),
+  CCE: z.object({
+    sampleSize1: z.number(),
+    sampleSize2: z.number(),
+    grainWeight: z.number(),
+    biomassWeight: z.number(),
+    cultivar: z.string(),
+    sowDate: z.date(),
+    harvestDate: z.date(),
+  }),
+  noOfImages: z.number(),
+});
 
-singleSyncRouter.post("/", (req, res) => {
-    res.json({
-        message: "succes",
-        dataId: 1
+singleSyncRouter.post("/", async (req, res) => {
+  console.log(req.body);
+  const body = req.body;
+
+  const result = await prisma.data.create({
+    data: {
+      latitude: body.latitude,
+      longitude: body.longitude,
+      accuracy: body.accuracyCorrection,
+      landCover: body.landCoverType,
+      description: body.locationDesc || " ",
+    //   userId: 2,
+      imageCount: body.noOfImages,
+      user: {
+        connect: { id: 2 },
+      },
+    },
+  });
+  if(req.body.landCoverType == "Cropland"){
+    const cropInformation = req.body.cropInformation
+    await prisma.cropInformation.create({
+        data: {
+            cropGrowthStage: cropInformation.cropGrowthStage,
+            cropIntensity: cropInformation.cropIntensity,
+            croppingPattern: cropInformation.croppingPattern,
+            livestock: cropInformation.liveStock,
+            primaryCrop: cropInformation.primarySeason.cropName,
+            primarySeason: cropInformation.primarySeason.seasonName,
+            remarks: cropInformation.remarks || " ",
+            secondaryCrop: cropInformation.secondarySeason.cropName,
+            secondarySeason: cropInformation.secondarySeason.seasonName,
+            waterSource: cropInformation.waterSource,
+            data: {
+                connect: {
+                    id: result.id
+                }
+            }
+        }
     })
-    console.log(req.body)
-})
+  }
+  if(req.body.CCE.isCaptured){
+    const CCEData = req.body.CCE;
+    await prisma.cCE.create({
+        data: {
+            biomassWeight: parseInt(CCEData.biomassWeight),
+            cultivar: CCEData.cultivar,
+            sowDate: new Date(CCEData.sowDate),
+            grainWeight:    parseInt(CCEData.grainWeight),
+            harvestDate: new Date(CCEData.harvestDate),
+            sampleSize_1: parseInt(CCEData.sampleSize1),
+            sampleSize_2: parseInt(CCEData.sampleSize2),
+            data: {
+                connect: {
+                    id: result.id
+                }
+            }
+        }
+    })
+  }
+  console.log(result);
+  res.json({
+    message: "succes",
+    dataId: 1,
+  });
+});
 
 singleSyncRouter.post("/image", (req, res) => {
-    const fileData = req.body.fileData;
-    // also intercept the extension of the file
-    const fileName = `${crypto.randomUUID()}.jpg`;
-    saveBase64File(fileData, fileName)
-    console.log(fileName, "dataid:", req.body.dataId)
-    res.send("response received")
-})
+  const fileData = req.body.fileData;
+  // also intercept the extension of the file
+  const fileName = `${crypto.randomUUID()}.jpg`;
+  saveBase64File(fileData, fileName);
+  console.log(fileName, "dataid:", req.body.dataId);
+  res.send("response received");
+});
 
-export default singleSyncRouter
+export default singleSyncRouter;
