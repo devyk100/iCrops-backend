31cbd9ab2a923f18b65037a442d36920769798e4
Your Name Here <devyk100@gmail.com>
2024-04-08 08:24:13 +0530

    some improvements

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,12 @@ import path from "node:path"
 const prisma = new PrismaClient();
 const app = express()
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://dit2dtt5nci8z.cloudfront.net/  ');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://dit2dtt5nci8z.cloudfront.net/  ');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 app.use(cors())
 app.use(express.json({
   limit: "50mb"
